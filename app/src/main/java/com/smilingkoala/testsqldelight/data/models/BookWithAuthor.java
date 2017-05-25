1e19daa0c0a5b3cf9feda403a52b172e584f12a2
Lukasz <luk.trojanowski@gmail.com>
2017-05-25 22:41:22 +0200

    example of sqlDelight problem

---
+++ app/src/main/java/com/smilingkoala/testsqldelight/data/models/BookWithAuthor.java
@@ -0,0 +1,8 @@
+package com.smilingkoala.testsqldelight.data.models;
+
+/**
+ * Created by Lukasz on 25.05.2017.
+ */
+
+abstract public class BookWithAuthor implements BookWithAuthorModel { // Cannot resolve symbol 'BookWithAuthorModel'
+}
